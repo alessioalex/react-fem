204e598ef4bf0d22e9f07f7d1930497a306953ea
Alexandru Vladutu <github@social.alessioalex.com>
2020-03-23 18:51:47 +0200

    wip, just after devtools

---
+++ adopt-me/src/SearchParams.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, setBreed] = useDropdown("breed", "", breeds);
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+      const breedStrings = apiBreeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]);
+  // how to run once ->
+  // }, []);
+  // /* useful for integrations, such as d3 stuff */
+
+  return (
+    <div className="search-params">
+      <h1>
+        {location} ({new Date().toLocaleTimeString()})
+      </h1>
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
